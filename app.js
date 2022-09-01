0f4601a77b9bf9cd484880d42ce4c8914ea241a6
astricklandd <alexxsriickland@gmail.com>
2022-09-01 13:22:05 -0500

    New changes to show index.html

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require('express')
+const path = require('node:path')
 const app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello. This is my new coding project')
+app.get('/', function (req, res) { //this line is an endpoint
+  //res.send('Hello. This is my new coding project')
+  // there is also a app.post function, but it is different than app.get
+  // ... below is how to postan html file called index.html 
+  res.sendFile(
+    path.join(__dirname, "index.html"));
+    // we searched this https://www.npmjs.com/package/nodemon after typing above line 
+    
 })
 
 app.listen(process.env.PORT ||3000,
-     console.log("Server is running"))
+     console.log("Server is running"));  
+
