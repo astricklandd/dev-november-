30d52493a08305f53b015ff2e3fe5b9c4c1e2f77
Strickland <alexxsriickland@gmail.com>
2022-09-06 13:09:20 -0500

    Newest changes

--- app.js
+++ app.js
@@ -1,30 +1,42 @@
+
 const express = require('express')
 const path = require('node:path')
+const bodyParser = require('body-parser')
+
 const app = express()
 
-app.set("niew engine", "ejs") //npm i ejs https://www.npmjs.com/package/ejs
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.set('view engine', 'ejs');
 
-app.get('/', function (req, res) { //this line is an endpoint
-  // res.send('Hello. This is my new coding project')
 
-  // there is also a app.post function, but it is different than app.get
-  // ... below is how to postan html file called index.html 
-  res.sendFile(
-   path.join(__dirname, "index.html"));
-    // we searched this https://www.npmjs.com/package/nodemon after typing above line 
-    
 
-  // res.send('Hello ' + userName + 'from Node/Express/Heroku.');
-  // res.send(`Hello ${userName} from Node/Express/Heroku with backticks!`);
-  // The two lines above cause an error
 
-  // BELOW is where we ended, mine did not work. Code from https://www.topcoder.com/thrive/articles/using-ejs-template-engine-with-express-js
-  // res.render('index', 
-  // {thatData}'
-  // userNameEJS: userNameExpress');
+
+
+let userName = "Joelz"; 
+let thatData = ""; 
+
+app.get('/', function (req, res) {
+    // res.sendFile(path.join(__dirname, "index.html" )); 
+    // res.send('Hello ' + userName + ' from Node/Express/Heroku');
+    // res.send(`Hello ${userName} from Node/Express/Heroku with Backticks!`)
+
+    res.render('index', 
+      { 
+        userName: userName
+      }
+      );
 
 })
 
-app.listen(process.env.PORT ||3000,
-     console.log("Server is running"));  
 
+app.post("/saveToNode", (req, res) => {
+  console.log(req.body);
+  console.group(req.body.userName);
+  res.render('index', { userName: req.body.userName });
+
+})
+
+app.listen(process.env.PORT || 3000,
+  () => console.log("Server is running..."));
